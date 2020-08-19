1e31319b2269dddec2c1652810e5c29820a5fa16
dong <3601778801@qq.com>
2020-08-20 01:33:10 +0800

    create project

---
+++ src/main/java/ge/cd/wind/WindApplication.java
@@ -0,0 +1,13 @@
+package ge.cd.wind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ge/cd/wind/WindApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.cd.wind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
